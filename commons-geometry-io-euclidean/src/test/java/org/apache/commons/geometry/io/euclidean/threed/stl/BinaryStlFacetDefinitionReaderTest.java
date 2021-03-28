c9afb9ca6928f03a9eabdaea6889e4cd80dafb68
Matt Juntunen <matt.juntunen@hotmail.com>
2021-03-28 08:07:07 -0400

    finishing BinaryStlFacetDefinitionReaderTest

--- commons-geometry-io-euclidean/src/test/java/org/apache/commons/geometry/io/euclidean/threed/stl/BinaryStlFacetDefinitionReaderTest.java
+++ commons-geometry-io-euclidean/src/test/java/org/apache/commons/geometry/io/euclidean/threed/stl/BinaryStlFacetDefinitionReaderTest.java
@@ -24,10 +24,14 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 import org.apache.commons.geometry.core.GeometryTestUtils;
+import org.apache.commons.geometry.euclidean.EuclideanTestUtils;
+import org.apache.commons.geometry.euclidean.threed.Vector3D;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BinaryStlFacetDefinitionReaderTest {
+
+    private static final double TEST_EPS = 1e-10;
 
     private static final String LONG_STRING =
             "A long string that will most definitely exceed the 80 byte length of the binary STL file format header.";
@@ -74,7 +78,6 @@ public class BinaryStlFacetDefinitionReaderTest {
         // arrange
         out.write(createHeader("Hello!", StandardCharsets.UTF_8, 1));
 
-        // act
         try (BinaryStlFacetDefinitionReader reader = new BinaryStlFacetDefinitionReader(getInput())) {
             // act/assert
             Assertions.assertEquals("Hello!", reader.getHeaderAsString());
@@ -87,7 +90,6 @@ public class BinaryStlFacetDefinitionReaderTest {
         // arrange
         out.write(createHeader(LONG_STRING, StandardCharsets.UTF_8, 8736720));
 
-        // act
         try (BinaryStlFacetDefinitionReader reader = new BinaryStlFacetDefinitionReader(getInput())) {
             // act/assert
             Assertions.assertEquals(LONG_STRING.substring(0, StlConstants.BINARY_HEADER_BYTES),
@@ -101,7 +103,6 @@ public class BinaryStlFacetDefinitionReaderTest {
         // arrange
         out.write(createHeader(LONG_STRING, StandardCharsets.UTF_16, 256));
 
-        // act
         try (BinaryStlFacetDefinitionReader reader = new BinaryStlFacetDefinitionReader(getInput())) {
             // act/assert
             Assertions.assertEquals("A long string that will most definitely",
@@ -150,6 +151,84 @@ public class BinaryStlFacetDefinitionReaderTest {
         }
     }
 
+    @Test
+    public void testReadFacet() throws IOException {
+        // arrange
+        out.write(createHeader(2));
+
+        out.write(getBytes(Vector3D.of(1, 2, 3)));
+        out.write(getBytes(Vector3D.of(4, 5, 6)));
+        out.write(getBytes(Vector3D.of(7, 8, 9)));
+        out.write(getBytes(Vector3D.of(10, 11, 12)));
+        out.write(getBytes((short) 1));
+
+        out.write(getBytes(Vector3D.of(-1, -2, -3)));
+        out.write(getBytes(Vector3D.of(-4, -5, -6)));
+        out.write(getBytes(Vector3D.of(-7, -8, -9)));
+        out.write(getBytes(Vector3D.of(-10, -11, -12)));
+        out.write(getBytes((short) 65535));
+
+        try (BinaryStlFacetDefinitionReader reader = new BinaryStlFacetDefinitionReader(getInput())) {
+            Assertions.assertEquals(2, reader.getNumTriangles());
+
+            final BinaryStlFacetDefinition facet1 = reader.readFacet();
+
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(1, 2, 3), facet1.getNormal(), TEST_EPS);
+            Assertions.assertEquals(3, facet1.getVertices().size());
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(4, 5, 6), facet1.getVertices().get(0), TEST_EPS);
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(7, 8, 9), facet1.getVertices().get(1), TEST_EPS);
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(10, 11, 12), facet1.getVertices().get(2), TEST_EPS);
+
+            Assertions.assertEquals(1, facet1.getAttributeValue());
+
+            final BinaryStlFacetDefinition facet2 = reader.readFacet();
+
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(-1, -2, -3), facet2.getNormal(), TEST_EPS);
+            Assertions.assertEquals(3, facet2.getVertices().size());
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(-4, -5, -6), facet2.getVertices().get(0), TEST_EPS);
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(-7, -8, -9), facet2.getVertices().get(1), TEST_EPS);
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(-10, -11, -12), facet2.getVertices().get(2), TEST_EPS);
+
+            Assertions.assertEquals(65535, facet2.getAttributeValue());
+
+            Assertions.assertNull(reader.readFacet());
+        }
+    }
+
+    @Test
+    public void testReadFacet_stopsWhenTriangleCountReached() throws IOException {
+        // arrange
+        out.write(createHeader(1));
+
+        out.write(getBytes(Vector3D.of(1, 2, 3)));
+        out.write(getBytes(Vector3D.of(4, 5, 6)));
+        out.write(getBytes(Vector3D.of(7, 8, 9)));
+        out.write(getBytes(Vector3D.of(10, 11, 12)));
+        out.write(getBytes((short) 1));
+
+        out.write(getBytes(Vector3D.of(-1, -2, -3)));
+        out.write(getBytes(Vector3D.of(-4, -5, -6)));
+        out.write(getBytes(Vector3D.of(-7, -8, -9)));
+        out.write(getBytes(Vector3D.of(-10, -11, -12)));
+        out.write(getBytes((short) 65535));
+
+        try (BinaryStlFacetDefinitionReader reader = new BinaryStlFacetDefinitionReader(getInput())) {
+            Assertions.assertEquals(1, reader.getNumTriangles());
+
+            final BinaryStlFacetDefinition facet = reader.readFacet();
+
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(1, 2, 3), facet.getNormal(), TEST_EPS);
+            Assertions.assertEquals(3, facet.getVertices().size());
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(4, 5, 6), facet.getVertices().get(0), TEST_EPS);
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(7, 8, 9), facet.getVertices().get(1), TEST_EPS);
+            EuclideanTestUtils.assertCoordinatesEqual(Vector3D.of(10, 11, 12), facet.getVertices().get(2), TEST_EPS);
+
+            Assertions.assertEquals(1, facet.getAttributeValue());
+
+            Assertions.assertNull(reader.readFacet());
+        }
+    }
+
     private ByteArrayInputStream getInput() {
         return new ByteArrayInputStream(out.toByteArray());
     }
@@ -164,11 +243,46 @@ public class BinaryStlFacetDefinitionReaderTest {
         final byte[] strBytes = str.getBytes(charset);
         System.arraycopy(strBytes, 0, result, 0, Math.min(StlConstants.BINARY_HEADER_BYTES, strBytes.length));
 
-        result[result.length - 4] = (byte) (count & 0x000000ff);
-        result[result.length - 3] = (byte) ((count & 0x0000ff00) >> 8);
-        result[result.length - 2] = (byte) ((count & 0x00ff0000) >> 16);
-        result[result.length - 1] = (byte) ((count & 0xff000000) >> 24);
+        final byte[] countBytes = getBytes(count);
+        System.arraycopy(countBytes, 0, result, StlConstants.BINARY_HEADER_BYTES, countBytes.length);
 
         return result;
+    }
+
+    private static byte[] getBytes(final Vector3D vec) throws IOException {
+        final byte[] result = new byte[Float.BYTES * 3];
+        int offset = 0;
+
+        System.arraycopy(getBytes((float) vec.getX()), 0, result, offset, Float.BYTES);
+        offset += Float.BYTES;
+
+        System.arraycopy(getBytes((float) vec.getY()), 0, result, offset, Float.BYTES);
+        offset += Float.BYTES;
+
+        System.arraycopy(getBytes((float) vec.getZ()), 0, result, offset, Float.BYTES);
+
+        return result;
+    }
+
+    private static byte[] getBytes(final float value) {
+        return getBytes(Float.floatToIntBits(value));
+    }
+
+    private static byte[] getBytes(final int value) {
+        final byte[] bytes = new byte[4];
+        bytes[0] = (byte) (value & 0x000000ff);
+        bytes[1] = (byte) ((value & 0x0000ff00) >> 8);
+        bytes[2] = (byte) ((value & 0x00ff0000) >> 16);
+        bytes[3] = (byte) ((value & 0xff000000) >> 24);
+
+        return bytes;
+    }
+
+    private static byte[] getBytes(final short value) {
+        final byte[] bytes = new byte[2];
+        bytes[0] = (byte) (value & 0x00ff);
+        bytes[1] = (byte) ((value & 0xff00) >> 8);
+
+        return bytes;
     }
 }
