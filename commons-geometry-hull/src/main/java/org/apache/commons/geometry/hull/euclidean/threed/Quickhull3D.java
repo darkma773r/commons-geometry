1b851d792e74654082c0750e751ef1d0b47caac9
Matt <mattjuntunen@apache.org>
2022-03-14 19:33:00 -0400

    start

---
+++ commons-geometry-hull/src/main/java/org/apache/commons/geometry/hull/euclidean/threed/Quickhull3D.java
@@ -0,0 +1,55 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.geometry.hull.euclidean.threed;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.geometry.euclidean.threed.Triangle3D;
+import org.apache.commons.geometry.euclidean.threed.Vector3D;
+
+public class Quickhull3D {
+
+    private static final class QuickhullFacet3D {
+
+        private final Triangle3D triangle;
+
+        private final List<Vector3D> outsidePoints = new ArrayList<>();
+
+        private Vector3D farthestOutsidePoint;
+
+        private double farthestOutsideDistance;
+
+        QuickhullFacet3D(final Triangle3D triangle) {
+            this.triangle = triangle;
+        }
+
+        public boolean addOutside(final Vector3D pt) {
+            final double dist = triangle.getPlane().offset(pt);
+            if (dist > 0.0) {
+                outsidePoints.add(pt);
+
+                if (dist > farthestOutsideDistance) {
+                    farthestOutsidePoint = pt;
+                }
+
+                return true;
+            }
+            return false;
+        }
+    }
+}
